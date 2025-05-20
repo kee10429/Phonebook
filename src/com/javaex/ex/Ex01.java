cdf188e664e8f4dd371d6c17fb7d28104e1783b6
kee10429 <hangijeong9@gmail.com>
2025-05-20 10:55:46 +0900

    10:55

--- src/com/javaex/ex/Ex01.java
+++ src/com/javaex/ex/Ex01.java
@@ -6,22 +6,40 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 
-import com.sun.tools.javac.util.List;
-
 public class Ex01 {
 
 	public static void main(String[] args) throws IOException{
 		
 		//리스트 만들기
-		List<Person> pList = new ArrayList<Person>();
+		java.util.List<Person> pList = new ArrayList<Person>();
 		//스트림 준비
-		Reader fr = new FileReader("C:\\javaStudy\\PhoneDB_원본.txt");
+		Reader fr = new FileReader("C:\\javaStudy\\미니프로젝트\\PhoneDB_원본.txt");
 		BufferedReader br = new BufferedReader(fr);
 		
 		//반복
 		while(true) {
+			String str = br.readLine();
 			
+			if(str==null) {
+				break;
+			}
+			
+			String[] sArray = str.split(",");
+			
+			String name = sArray[0];
+			String hp = sArray[1];
+			String company = sArray[2];
+			
+			Person p = new Person(name, hp, company);
+			pList.add(p);
 		}
+		
+		//전체값 출력
+		System.out.println(pList.toString());
+		
+		
+		//닫기
+		br.close();
 		
 		
 	}
